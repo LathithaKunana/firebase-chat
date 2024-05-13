50a0e343594d4a95c56e469e15d61237d7507049
LathithaKunana <lathitha.kunana@younglings.africa>
2024-05-13 13:50:42 +0200

    completed the ui for the register page now i am busy with the firebase configeration

--- app/signIn.js
+++ app/signIn.js
@@ -1,13 +1,30 @@
-import { View, Text, Image, TextInput, TouchableOpacity, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity, Pressable, Alert } from 'react-native'
+import React, { useRef, useState } from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { StatusBar } from 'expo-status-bar';
 import { Octicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import Loading from '../components/Loading';
+import CustomKeyboardView from '../components/CustomKeyboardView';
 
 
 export default function SignIn() {
+  const router = useRouter();
+  const [loading, setloading] = useState(false)
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const handleLogin = async() => {
+    if(!emailRef.current || !passwordRef.current){
+      Alert.alert('Sign In', 'Please fill all the fields.')
+    } else {
+      //login process
+    }
+  }
+
   return (
-    <View className='flex-1 '>
+    <CustomKeyboardView>
       <StatusBar style='dark' />
       <View style={{paddingTop: hp(8)}} className="flex-1 space-y-12">
         {/* signIn image */}
@@ -23,6 +40,7 @@ export default function SignIn() {
             <View style={{height:hp(7), width:hp(40)}} className="flex-row space-x-4 px-4 bg-neutral-100 items-center rounded-2xl">
               <Octicons name='mail' size={hp(2.7)} color={"gray"} />
               <TextInput
+              onChangeText={value => emailRef.current = value}
                   style={{fontSize: hp(2)}}
                   className="flex-1 font-semibold text-neutral-700 justify-center"
                   placeholder='Email address'
@@ -33,8 +51,10 @@ export default function SignIn() {
               <View style={{height:hp(7), width:hp(40)}} className="flex-row space-x-4 px-4 bg-neutral-100 items-center rounded-2xl">
                 <Octicons name='lock' size={hp(2.7)} color={"gray"} />
                 <TextInput
+                    onChangeText={value => passwordRef.current = value}
                     style={{fontSize: hp(2)}}
                     className="flex-1 font-semibold text-neutral-700 justify-center"
+                    secureTextEntry
                     placeholder='Password'
                     placeholderTextColor={'gray'}
                 />    
@@ -43,15 +63,26 @@ export default function SignIn() {
             <Text style={{fontSize: hp(1.8)}} className="font-semibold text-right text-neutral-500">Forgot password</Text>
 
             {/* submit button */}
-            <TouchableOpacity style={{height: hp(6.5), backgroundColor: "#006F5E"}} className="rounded-xl justify-center items-center">
-              <Text style={{fontSize: hp(2.7)}} className = "text-white font-bold tracking-wider">Sign In</Text>
-            </TouchableOpacity>
+            <View>
+              {
+                loading? (
+                  <View className="flex-row justify-center">
+                    <Loading size={hp(10)} />
+                  </View>
+                ):(
+                  <TouchableOpacity onPress={handleLogin} style={{height: hp(6.5), backgroundColor: "#006F5E"}} className="rounded-xl justify-center items-center">
+                    <Text style={{fontSize: hp(2.7)}} className = "text-white font-bold tracking-wider">Sign In</Text>
+                  </TouchableOpacity>
+                )
+              }
+            </View>
+            
 
             {/* sign up text */}
 
             <View className=" flex-row justify-center">
               <Text style={{fontSize: hp(1.8)}} className="font-semibold text-neutral-500">Dont have an account? </Text>
-              <Pressable>
+              <Pressable onPress={() => router.push('signUp')}>
                 <Text style={{fontSize: hp(1.8), color: "#006F5E"}} className="font-bold">Sign Up </Text>
               </Pressable>
             </View>
@@ -59,6 +90,6 @@ export default function SignIn() {
           </View>
         </View>
       </View>
-    </View>
+    </CustomKeyboardView>
   )
 }
---
+++ components/CustomKeyboardView.js
@@ -0,0 +1,24 @@
+import { View, Text, Platform, KeyboardAvoidingView, ScrollView } from 'react-native'
+import React from 'react'
+
+const ios = Platform.OS == 'ios';
+
+export default function CustomKeyboardView({children}) {
+  return (
+    <KeyboardAvoidingView
+        behavior={ios? 'padding': 'height'}
+        style={{flex: 1}}
+    >
+        <ScrollView
+            style={{flex: 1}}
+            bounces={false}
+            showsVerticalScrollIndicator={false}
+        >
+            {
+                children
+            }
+
+        </ScrollView>
+    </KeyboardAvoidingView>
+  )
+}
